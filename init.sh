3155ecf892d08339be0145c9f1cd8793b32baaf6
Mark Farnsworth <farnsworth2008@gmail.com>
2019-02-20 09:58:17 -0500

    Perform eclipse task on init

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ set -e
 git clone git@github.com:farnsworth2008/moar-sugar
 cp -r moar-sugar/gradle .
 cp moar-sugar/gradlew .
-./gradlew build
+./gradlew build eclipse
 cd moar-sugar/cli
 npm install babel-register babel-preset-env --save
 npm run-script build
