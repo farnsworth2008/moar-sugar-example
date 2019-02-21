003497276ac86a37b8f7f8acfaa2cbb46a2fc1b8
Mark Farnsworth <farnsworth2008@gmail.com>
2019-02-21 13:39:21 -0500

    Improved comments in examples

--- moar-sugar-app/src/main/java/moar/sugar/example/AsyncExample.java
+++ moar-sugar-app/src/main/java/moar/sugar/example/AsyncExample.java
@@ -1,7 +1,7 @@
 package moar.sugar.example;
 
 import static java.lang.String.format;
-import static moar.sugar.Sugar.safely;
+import static moar.sugar.Sugar.require;
 import static moar.sugar.Sugar.swallow;
 import static moar.sugar.thread.MoarThreadSugar.$;
 import java.io.PrintStream;
@@ -16,40 +16,43 @@ class AsyncExample
 
   @Override
   public void run() {
+
     out.println("Example: Async Execution");
-    var service = $(4);
-    try {
-      var futures = $(String.class);
 
-      for (var i = 0; i < 3; i++) {
-        var message = "async " + i;
-        $(service, futures, () -> methodOne(message));
+    /* [require] shorthand to require everything in the block to complete
+     * without exception. */
+    require(() -> {
+
+      /* $ shorthand for a service using 4 threads. */
+      try (var service = $(4)) {
+
+        /* $ shorthand for a future where we get a string. */
+        var futures = $(String.class);
+
+        /* $ shorthand to run lambda(s) async */
+        for (var i = 0; i < 3; i++) {
+          var message1 = format("async One [%d]", i);
+          var message2 = format("async Two [%d]", i);
+          var message3 = format("async Three [%d]", i);
+          $(service, futures, () -> methodOne(message1));
+          $(service, futures, () -> methodTwo(message2), () -> methodTwo(message3));
+        }
+
+        /* $ shorthand to wait for all futures to complete */
+        out.println("  async work started");
+        swallow(() -> $(futures));
+        out.println("  async work complete");
+
+        /* $ shorthand to get a safe result from a future */
+        var i = 0;
+        for (var future : futures) {
+          var result = $(future);
+          var futureThrew = result.thrown() == null;
+          var displayValue = futureThrew ? result.get() : result.thrown().getMessage();
+          out.println(format("  futures[%d]: %s", ++i, displayValue));
+        }
       }
-
-      for (var i = 0; i < 3; i++) {
-        var message = "async " + i;
-        $(service, futures, () -> methodTwo(message));
-      }
-
-      for (var i = 0; i < 3; i++) {
-        var message = "async " + i;
-        $(service, futures, () -> methodWithRetryableException(message));
-      }
-
-      out.println("  async work started");
-      swallow(() -> $(futures));
-      out.println("  async work complete");
-
-      var i = 0;
-      for (var future : futures) {
-        var result = safely(() -> future.get());
-        var displayValue = result.thrown() == null ? result.get() : result.thrown().getMessage();
-        out.println(format("  futures[%d]: %s", ++i, displayValue));
-      }
-
-    } finally {
-      service.shutdown();
-    }
+    });
     out.println();
   }
 
--- moar-sugar-app/src/main/java/moar/sugar/example/DatabaseExample.java
+++ moar-sugar-app/src/main/java/moar/sugar/example/DatabaseExample.java
@@ -15,14 +15,17 @@ class DatabaseExample
 
   @Override
   public void run() {
+
+    /* Works with standard javax.sql.DataSource */
     var ds = getDataSource();
 
     out.println("Example: DB");
 
-    // simple way to executeSql against a DataSource
+    /* Simple way to executeSql with connection open and close handled
+     * automatically */
     wake(ds).executeSql("delete from pet");
 
-    // style 1: Upsert using a fully fluent style
+    /* Fluent syntax style without the need for a repository of each type. */
     var pet1 = wake(PetRow.class).of(ds).upsert(row -> {
       row.setName("Donut");
       row.setOwner("Mark");
@@ -32,7 +35,7 @@ class DatabaseExample
     });
     out.println("  upsert pet #1: " + pet1.getId() + ", " + pet1.getName());
 
-    // style 2: Upsert using style where we hold the repository reference.
+    /* Repository of each type can also be passed around. */
     var repo = wake(PetRow.class).of(ds);
     PetRow pet2 = repo.define();
     pet2.setName("Tig");
@@ -42,35 +45,34 @@ class DatabaseExample
     pet2.setBirth(toUtilDate(2018, 4, 22));
     repo.upsert(pet2);
     out.println("  upsert pet #2: " + pet2.getId() + ", " + pet2.getName());
-    Long pet2Id = pet2.getId();
+    var pet2Id = pet2.getId();
 
-    // Find with ID and update
+    /* Find based on ID is very simple and update */
     var foundPet = repo.id(pet2Id).find();
     out.println("  found: " + foundPet.getName());
+
+    /* Update is simply provided by the repo */
     foundPet.setOwner("Mark");
     repo.update(foundPet);
 
-    // Find with a key
-    foundPet = repo.key(r -> {
-      r.setName("Tig");
-      r.setSpecies("Dog");
+    // Find rows using an example row to search
+    foundPet = repo.key(where -> {
+      where.setName("Tig");
+      where.setSpecies("Dog");
     }).find();
     out.println("  found: " + foundPet.getName() + ", " + foundPet.getOwner());
 
     // Delete
     repo.delete(foundPet);
 
-    // Upsert to add row for query
-    repo.upsert(row -> {
+    // Upsert multiple rows.
+    wake(ds).upsert(PetRow.class, row -> {
       row.setName("Twyla");
       row.setOwner("Kendra");
       row.setSex("F");
       row.setSpecies("Cat");
       row.setBirth(toUtilDate(2012, 6, 5));
-    });
-
-    // Upsert to add row for query
-    repo.upsert(row -> {
+    }, row -> {
       row.setName("Jasper");
       row.setOwner("Kendra");
       row.setSex("M");
